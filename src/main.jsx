cac2d371df8bc04922796fb39820d2097f2d39d6
nadityab <dewaadit14@gmail.com>
2026-04-21 23:42:22 +0700

    Dark Mode Test

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Tambahkan ini
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext"; // 👈 1. Impor ini
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      {/* 👈 2. Bungkus App dengan ThemeProvider */}
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
